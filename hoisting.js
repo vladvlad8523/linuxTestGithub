b51b2d9d8fe68c8c444c443d06f67c459eb986c2
Vladislav <vlad.iliuk@gmail.com>
2023-09-23 21:22:30 +0300

    const ir let

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+// console.log(sum(1, 41))
+// // pries funkcija isvedimas
+// function sum(a, b){
+//     return a+ b
+// }
+
+console.log(sum)
+const sum = 42 // jei pakeist i let bus klaida
+console.log(sum)
