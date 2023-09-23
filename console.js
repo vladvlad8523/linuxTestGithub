dda7972e092addd4f8afc05ac8ee8dc3a1e6b610
Vladislav <vlad.iliuk@gmail.com>
2023-09-23 19:33:12 +0300

    add typeof tree

--- console.js
+++ console.js
@@ -6,3 +6,8 @@ sum = num1 + num2;
 
 console.log(sum);
 console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 'javascript'); //vienodi: "", '', literalai `` ( nauduojama su dinaminiais duomenimis)
+console.log(typeof undefined);
+console.log(typeof Math);
+console.log(typeof Symbol('JS'));
