ad74cd99c425411d2893e41357165ee71154641b
Vladislav <vlad.iliuk@gmail.com>
2023-09-23 19:53:54 +0300

    add boolean comment

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ if (language) { //if = true
     console.log('the best language is: ', language)
 }
 //false reiksme: '', null, NaN, undefined, false;
+console.log(Boolean(''))
+console.log(Boolean('Hi'))
+console.log(Boolean(' '))
