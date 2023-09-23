fdf8559dc2191f57baaa13c20535acdd8a31df5f
Vladislav <vlad.iliuk@gmail.com>
2023-09-23 20:10:04 +0300

    del sekos su string

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ console.log(Boolean(' ')) // tarpas ne tuscias todel true, nes symbolis
 console.log(Boolean([])) //masyvas true
 console.log(Boolean({})) //objectas true
 console.log(Boolean(1 + '2')) //konkotinacija
+console.log(Boolean('' + 1 + '0')) //konkotinacija = string 10
+console.log(Boolean('1' * '2')) //konkotinacija neveiks, nes yra *
+console.log(Boolean(4 + 5 + 'px')) //bus 9 nes pirminis sk. o string antras
+console.log(Boolean('' + 4 +5)) //pirminis tring result = 45
