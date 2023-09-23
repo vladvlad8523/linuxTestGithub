0369cd7dab66168efd5c1cd154a64cdc9eac9a01
Vladislav <vlad.iliuk@gmail.com>
2023-09-23 21:29:33 +0300

    add square function

--- hoisting.js
+++ hoisting.js
@@ -4,6 +4,12 @@
 //     return a+ b
 // }
 
-console.log(sum)
-const sum = 42 // jei pakeist i let bus klaida
-console.log(sum)
+// console.log(sum)
+// const sum = 42 // jei pakeist i let bus klaida
+// console.log(sum)
+
+// Function declaration & Function Expression
+console.log(square(25))
+function square(num) {
+    return num**2
+}
